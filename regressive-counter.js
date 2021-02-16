ebbc13e46204a92732d1431e758a6c5dd12402fe
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2021-02-16 18:36:24 -0300

    Add files via upload

---
+++ regressive-counter.js
@@ -0,0 +1,42 @@
+var timeLeft, finalDate, milliseconds, day, seconds, minutes, now, hours, days;
+day = 19;
+
+finalDate = new Date();
+finalDate.setDate(day);
+finalDate.setHours(12);
+finalDate.setMinutes(0);
+finalDate.setSeconds(0);
+
+milliseconds = Date.parse(finalDate);
+
+var timer = setInterval(function() {
+	now = new Date();
+	
+	if(finalDate.getDate() === now.getDate() && finalDate.getHours() === now.getHours() && finalDate.getMinutes() === now.getMinutes()) {
+		var newDay = day >= 1 && day <= 31 ? day + 3 : day=1;
+
+		finalDate = new Date();
+		finalDate.setDate(newDay);
+		finalDate.setHours(12);
+		finalDate.setMinutes(0);
+		finalDate.setSeconds(0);
+		milliseconds = Date.parse(finalDate);
+	};
+
+	dateDiff = milliseconds - Date.parse(Date());
+	timeLeft = new Date(dateDiff);
+
+	seconds = Math.floor((timeLeft / 1000) % 60);
+	minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+	hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+	days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+
+	$('.timer .count-timer .days').text(addLeftZero(days));
+	$('.timer .count-timer .hours').text(addLeftZero(hours));
+	$('.timer .count-timer .minutes').text(addLeftZero(minutes));
+	$('.timer .count-timer .seconds').text(addLeftZero(seconds));
+}, 1000)
+
+function addLeftZero(number) {
+	return number < 10 ? '0' + number : number;
+}

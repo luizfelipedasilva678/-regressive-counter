d3dd96a3b6aa848862de34c5306cb6deeb1b862d
Luiz Felipe <40125808+luizfelipedasilva678@users.noreply.github.com>
2021-02-22 14:19:50 -0300

    Update regressive-counter.js

--- regressive-counter.js
+++ regressive-counter.js
@@ -1,43 +1,49 @@
-var timeLeft, finalDate, milliseconds, day, seconds, minutes, now, hours, days;
-day = 19;
+var timeLeft, finalDate, milliseconds, day, seconds, minutes, now, hours, days, newDay;
+	day = 19;
 
-finalDate = new Date();
-finalDate.setDate(day);
-finalDate.setHours(12);
-finalDate.setMinutes(0);
-finalDate.setSeconds(0);
-milliseconds = Date.parse(finalDate);
+	finalDate = new Date();
+	finalDate.setDate(day);
+	finalDate.setHours(12);
+	finalDate.setMinutes(0);
+	finalDate.setSeconds(0);
+	milliseconds = Date.parse(finalDate);
+	
+	var timer = setInterval(function() {
+		now = new Date();
+		
+		dateDiff = milliseconds - Date.parse(Date());
+		timeLeft = new Date(dateDiff);
+		
+		seconds = Math.floor((timeLeft / 1000) % 60);
+		minutes = Math.floor((timeLeft / 1000 / 60) % 60);
+		hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
+		days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+		
+		if(days < 0 && minutes < 0 && hours < 0 && seconds < 0) {	
+			if(day >=1 && day <=31) {
+				day += 3;
+				newDay = day;
+			} else {
+				day = 1;
+				newDay = day;
+			}
 
-var timer = setInterval(function() {
-	now = new Date();
+			finalDate = new Date();
+			finalDate.setDate(newDay);
+			finalDate.setHours(12);
+			finalDate.setMinutes(0);
+			finalDate.setSeconds(0);
+			milliseconds = Date.parse(finalDate);
+		}
 
-	dateDiff = milliseconds - Date.parse(Date());
-	timeLeft = new Date(dateDiff);
+		if(days >= 0 && minutes >= 0 && hours >= 0 && seconds >= 0) {
+			$('.timer .count-timer .days').text(addLeftZero(days));
+			$('.timer .count-timer .hours').text(addLeftZero(hours));
+			$('.timer .count-timer .minutes').text(addLeftZero(minutes));
+			$('.timer .count-timer .seconds').text(addLeftZero(seconds));
+		}
+	}, 1000)
 
-	seconds = Math.floor((timeLeft / 1000) % 60);
-	minutes = Math.floor((timeLeft / 1000 / 60) % 60);
-	hours = Math.floor((timeLeft / (1000 * 60 * 60)) % 24);
-	days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-
-	if(days < 0 && minutes < 0 && hours < 0 && seconds < 0) {
-		var newDay = day >= 1 && day <= 31 ? day + 3 : day=1;
-
-		finalDate = new Date();
-		finalDate.setDate(newDay);
-		finalDate.setHours(12);
-		finalDate.setMinutes(0);
-		finalDate.setSeconds(0);
-		milliseconds = Date.parse(finalDate);
-	}
-
-	if(days >= 0 && minutes >= 0 && hours >= 0 && seconds >= 0) {
-		$('.timer .count-timer .days').text(addLeftZero(days));
-		$('.timer .count-timer .hours').text(addLeftZero(hours));
-		$('.timer .count-timer .minutes').text(addLeftZero(minutes));
-		$('.timer .count-timer .seconds').text(addLeftZero(seconds));
-	}
-}, 1000)
-
-function addLeftZero(number) {
-	return number < 10 ? '0' + number : number;
-}
+	function addLeftZero(number) {
+        return number < 10 ? '0' + number : number;
+    }
